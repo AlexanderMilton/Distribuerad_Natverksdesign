215e687010fe61b64b824730b4c0d626627d1ed6
alexandermilton <alexandermiltons@gmail.com>
2015-01-19 16:02:23 +0100

    Opened repo

---
+++ Hello World/src/HeyoWorld.java
@@ -0,0 +1,11 @@
+/*
+Hello World.
+In the main file, prints a string file to the system.
+*/
+
+public class HeyoWorld {
+	public static void main (String[] arg) {
+		System.out.println("Hello World by b13alemi");
+	}
+}
+
